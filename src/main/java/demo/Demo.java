94cddce6f202a6683ac9c0c6820f548ae9ef35ba
MartinBG <MartinBG@abv.bg>
2022-07-05 14:12:50 +0300

    refactor to switch expression

--- src/main/java/demo/Demo.java
+++ src/main/java/demo/Demo.java
@@ -3,13 +3,10 @@ package demo;
 public class Demo {
 
     public String greeting(String from) {
-        switch (from) {
-        case "Spock":
-            return "Hello from Spock!";
-        case "JUnit 5":
-            return "Hello from JUnit 5!";
-        default:
-            throw new IllegalArgumentException("Invalid name " + from);
-        }
+        return switch (from) {
+            case "Spock" -> "Hello from Spock!";
+            case "JUnit 5" -> "Hello from JUnit 5!";
+            default -> throw new IllegalArgumentException("Invalid name " + from);
+        };
     }
 }
